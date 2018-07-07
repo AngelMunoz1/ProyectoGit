6ff7038fdb5997fab6dd416215b43f412811c20f
AngelMunoz18 <amgelmc@unikino.edu.mx>
2018-07-07 16:53:29 -0500

    SubirTodo

--- ProyectoGit/Form1.Designer.cs
+++ ProyectoGit/Form1.Designer.cs
@@ -28,20 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rtb1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgv1
+            // 
+            this.dgv1.AllowUserToAddRows = false;
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dgv1.Location = new System.Drawing.Point(41, 186);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.ReadOnly = true;
+            this.dgv1.Size = new System.Drawing.Size(343, 150);
+            this.dgv1.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Codigo";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Nombre";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Precio";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // rtb1
+            // 
+            this.rtb1.Location = new System.Drawing.Point(41, 28);
+            this.rtb1.Name = "rtb1";
+            this.rtb1.Size = new System.Drawing.Size(343, 129);
+            this.rtb1.TabIndex = 2;
+            this.rtb1.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(507, 302);
+            this.ClientSize = new System.Drawing.Size(604, 439);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.rtb1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.RichTextBox rtb1;
     }
 }
 
--- ProyectoGit/Form1.cs
+++ ProyectoGit/Form1.cs
@@ -12,18 +12,60 @@ namespace ProyectoGit
 {
     public partial class Form1 : Form
     {
-
-        string Res ="";
-        int a = 1;
+        private List<Productos> listaProductos = new List<Productos>();
+        Productos[] producto = new Productos[3];
         public Form1()
         {
-            InitializeComponent();
-            Res = Convert.ToString(a);
-            MessageBox.Show("HOLA MUNDO");
-            MessageBox.Show("HOLA MUNDO");
+            InitializeComponent();           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Productos producto = new Productos();
+            //producto.pCodigo = 9123456789;
+            //producto.pNombre = "Miller";
+            //producto.pPrecio = 36.00f;
+            //rtb1.Text += producto.pCodigo + Environment.NewLine + producto.pNombre + Environment.NewLine + producto.pPrecio;
+            //Comentario nuevo
+            producto[0] = new Productos(1, "producto 1", 35.00f);
+            producto[1] = new Productos(2, "producto 2", 35.02f);
+            producto[2] = new Productos(3, "producto 3", 35.08f);
+            cargaProducto();
+
+        }
+        private void cargaProducto()
+        {
+            try
+            {
+                for (int i = 0; i < producto.Length; i++)
+                {
+                    dgv1.Rows.Add(producto[i].pCodigo, producto[i].pNombre, producto[i].pPrecio);
+                }
+            }
+            catch (Exception err)
+            {
+
+                MessageBox.Show("ERROR:" + Environment.NewLine + err);
+            }
         }
     }
 
-    
+    class Productos
+    {
+        //Propiedades
+        public long pCodigo = 0;
+        public String pNombre = "";
+        public float pPrecio = 0.00f;
 
+        public Productos()
+        {
+            MessageBox.Show("Me llamaron sin parametros");
+        }
+        public Productos(long pCodigo, String pNombre, float pPrecio)
+        {
+            this.pCodigo = pCodigo;
+            this.pNombre = pNombre;
+            this.pPrecio = pPrecio;
+        }
+    }
 }
